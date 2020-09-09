b9543523958ede570270790a7f25cc0feb41ad82
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-09-09 18:39:12 +0530

    email pattern is added

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -5,12 +5,12 @@
 #constants
 firstNamePattern=^[[:upper:]]{1,}[A-Za-z]{2,}$
 lastNamePattern=^[[:upper:]]{1,}[A-Za-z]{2,}$
-
+emailPattern="^[[:lower:]]{3}[a-zA-Z0-9]*([-+.]?[a-zA-Z0-9])*@[a-z]*.[a-z]{2,3}([.][a-z]{2,3})*$"
 
 #inputs
 read -p "enter first name : " firstName
 read -p "enter last name : "  lastName
-
+read -p "enter the email : " email
 
 #checking conditions
 if [[ $firstName =~ $firstNamePattern ]]
@@ -26,3 +26,12 @@ then
 else
         echo invalid last name pattern
 fi
+
+if [[ $email =~ $emailPattern ]]
+then
+	echo valid email pattern
+else
+	echo invalid email pattern
+fi
+
+
