e411230d61ac4c6d280eb15b0a077910ef0b12e9
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-09-09 16:23:59 +0530

    adding last name feature is added

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,20 +1,28 @@
-#! /bin/bash -x
+#! /bin/bash
 # User Registration
 
 
 #constants
 firstNamePattern=^[[:upper:]]{1,}[A-Za-z]{2,}$
+lastNamePattern=^[[:upper:]]{1,}[A-Za-z]{2,}$
 
 
+#inputs
+read -p "enter first name : " firstName
+read -p "enter last name : "  lastName
 
 
-echo "enter first name"
-
-read firstName
-
+#checking conditions
 if [[ $firstName =~ $firstNamePattern ]]
 then
-	echo valid username
+	echo valid first name pattern
 else
-	echo invalid username
+	echo invalid first name pattern
+fi
+
+if [[ $lastName =~ $lastNamePattern ]]
+then
+        echo valid last name pattern
+else
+        echo invalid last name pattern
 fi
