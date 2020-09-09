a334d145f66d77e319298c67d000296793372591
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-09-09 18:47:49 +0530

    mobile pattern is added

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -6,11 +6,16 @@
 firstNamePattern=^[[:upper:]]{1,}[A-Za-z]{2,}$
 lastNamePattern=^[[:upper:]]{1,}[A-Za-z]{2,}$
 emailPattern="^[[:lower:]]{3}[a-zA-Z0-9]*([-+.]?[a-zA-Z0-9])*@[a-z]*.[a-z]{2,3}([.][a-z]{2,3})*$"
+mobilePattern="^[0-9]{2}[[:space:]][0-9]{10}$"
+
+
+
 
 #inputs
 read -p "enter first name : " firstName
 read -p "enter last name : "  lastName
 read -p "enter the email : " email
+read -p "enter the mobile number : " mobile
 
 #checking conditions
 if [[ $firstName =~ $firstNamePattern ]]
@@ -34,4 +39,9 @@ else
 	echo invalid email pattern
 fi
 
-
+if [[ $mobile =~ $mobilePattern ]]
+then
+	echo valid mobile pattern
+else
+	echo invalid mobile pattern
+fi
