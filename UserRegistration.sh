cab74310b26320a585e9abf04c04e2737e4aef20
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-09-09 19:15:58 +0530

    password check 1

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -7,7 +7,7 @@ firstNamePattern=^[[:upper:]]{1,}[A-Za-z]{2,}$
 lastNamePattern=^[[:upper:]]{1,}[A-Za-z]{2,}$
 emailPattern="^[[:lower:]]{3}[a-zA-Z0-9]*([-+.]?[a-zA-Z0-9])*@[a-z]*.[a-z]{2,3}([.][a-z]{2,3})*$"
 mobilePattern="^[0-9]{2}[[:space:]][0-9]{10}$"
-
+passwordPattern="[a-zA-Z0-9!@#$%^&*()-_+.,]{8,}"
 
 
 
@@ -16,6 +16,7 @@ read -p "enter first name : " firstName
 read -p "enter last name : "  lastName
 read -p "enter the email : " email
 read -p "enter the mobile number : " mobile
+read -p "enter the password : " password
 
 #checking conditions
 if [[ $firstName =~ $firstNamePattern ]]
@@ -44,4 +45,11 @@ then
 	echo valid mobile pattern
 else
 	echo invalid mobile pattern
+fi
+
+if [[ $password =~ $passwordPattern ]]
+then
+	echo valid password pattern
+else
+	echo invalid password pattern
 fi
