af96c35c314eb22ac38a04190c42ff1e6d12a253
TarunVyda6 <kamaltarun.rao6@gmail.com>
2020-09-09 16:12:09 +0530

    added username feature

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,20 @@
-#! /bin/bash
+#! /bin/bash -x
 # User Registration
+
+
+#constants
+firstNamePattern=^[[:upper:]]{1,}[A-Za-z]{2,}$
+
+
+
+
+echo "enter first name"
+
+read firstName
+
+if [[ $firstName =~ $firstNamePattern ]]
+then
+	echo valid username
+else
+	echo invalid username
+fi
